c742ad018902b03d5319c30091834b8045c79244
HCross <haydenlcrossjunk@gmail.com>
2019-12-10 19:07:03 -0800

    added materialize cards to HTML

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ $('document').ready(function () {
     //var apiKey = " d0aef524cfc14d6ba3f35bc68ab620b9"; //FGuzman
     //var apiKey = "06238180649d43e0bffc9f3ac6536dc3"; //HCross
     //var apiKey = "5aac1a10cd874816809acc6f2d2fa006"; //FOrtiz
-    //var apiKey = "bb5452cb4b074d1a899410830c863f29"; //Emily
-    var apiKey = "d453036a9eeb46a1b474c7043973a767"; //xapienx.com
+    var apiKey = "bb5452cb4b074d1a899410830c863f29"; //Emily
+    //var apiKey = "d453036a9eeb46a1b474c7043973a767"; //xapienx.com
     //var apiKey = " f4abc8a8916747b3a3976addc1321ab0;" //birulaplanet.com
     //var apiKey = " 0421115dd3974c7f9338166f3e907824;" // Emily2
 
